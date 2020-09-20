6937f892a78b8bc284e976b8e2df54ed77265dc8
fixiabis <fixiabis@gmail.com>
2020-09-20 22:15:35 +0800

    feat: add tag side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import React, { useState, useCallback } from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Tag, Input, Space, Divider } from 'antd';
 import {
   FileOutlined,
   FolderOutlined,
   FolderAddOutlined,
+  TagOutlined,
+  PlusOutlined,
 } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import { MenuProps } from 'antd/lib/menu';
 
-const { Sider } = Layout;
+const { Sider, Content, Header } = Layout;
 const { SubMenu } = Menu;
+const { CheckableTag } = Tag;
 
 function App() {
-  const [collapsed, setCollapsed] = useState(false);
+  const [fileSiderCollapsed, setFileSiderCollapsed] = useState(false);
+  const [tagsSiderCollapsed, setTagsSiderCollapsed] = useState(false);
+
   const [
     directoryHandle,
     setDirectoryHandle,
   ] = useState<FileSystemDirectoryHandle | null>(null);
+
   const [fileHandles, setFileHandles] = useState<FileSystemFileHandle[] | null>(
     null
   );
@@ -50,7 +56,11 @@ function App() {
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+      <Sider
+        collapsible
+        collapsed={fileSiderCollapsed}
+        onCollapse={setFileSiderCollapsed}
+      >
         <Menu
           theme="dark"
           defaultSelectedKeys={['-1']}
@@ -75,6 +85,43 @@ function App() {
               ))}
             </SubMenu>
           )}
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header style={{ background: '#fff' }}></Header>
+        <Content></Content>
+      </Layout>
+      <Sider
+        theme="light"
+        collapsible
+        collapsed={tagsSiderCollapsed}
+        onCollapse={setTagsSiderCollapsed}
+        reverseArrow
+      >
+        <div style={{ padding: 10 }}>
+          <Input.Search enterButton placeholder="search tags" />
+        </div>
+        <Divider style={{ margin: 0 }} />
+        <div style={{ padding: 10 }}>
+          找不到自己加:
+          <Input.Search
+            placeholder="add group"
+            enterButton={<PlusOutlined />}
+          />
+        </div>
+        <Menu theme="light" mode="inline">
+          <SubMenu key="tag-group-1" title="tag group 1" icon={<TagOutlined />}>
+            <Menu.Item style={{ height: 'auto' }}>
+              <Space style={{ flexWrap: 'wrap' }}>
+                <CheckableTag checked>tag 1</CheckableTag>
+                <CheckableTag checked={false}>tag 2</CheckableTag>
+                <Tag className="site-tag-plus">
+                  <PlusOutlined />
+                  New
+                </Tag>
+              </Space>
+            </Menu.Item>
+          </SubMenu>
         </Menu>
       </Sider>
     </Layout>
